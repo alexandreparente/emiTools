a7e12788f3cd0daedf49d08ada4385b450e015f8
alexandreparente <alexandre.parente@gmail.com>
2024-10-11 23:47:41 -0300

    Build

---
+++ zip_build/emi_tools/emi_tools_export_kml_rpa.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsVectorFileWriter,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterField,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingMultiStepFeedback,
+                       QgsProcessingParameterFile,
+                       QgsProcessingParameterFolderDestination,
+                       QgsProject,
+                       QgsVectorLayer,
+                       QgsFeature,
+                       QgsProcessingException,
+                       QgsWkbTypes)
+
+import os
+import zipfile
+
+def tr(string):
+    return QCoreApplication.translate('@default', string)
+
+class emiToolsExportKmlRpa(QgsProcessingAlgorithm):
+
+    output_folder = 'output_folder'
+
+    def initAlgorithm(self, config=None):
+        # Input layer parameter using QgsProcessingParameterFeatureSource
+        self.addParameter(QgsProcessingParameterFeatureSource('layer', tr('Layer name:'), 
+                    [QgsProcessing.TypeVectorPolygon, QgsProcessing.TypeVectorLine]))
+        
+        # Input field parameter to select a field for naming the exported files
+        self.addParameter(QgsProcessingParameterField('export_field', tr('Field to use for export file names:'), '', 'layer', optional=True))
+
+        # Output folder parameter
+              
+        default_output_folder = os.path.expanduser("~")      
+        self.addParameter(QgsProcessingParameterFolderDestination(self.output_folder, tr('Output folder'), defaultValue=default_output_folder))
+        
+#        self.addParameter(QgsProcessingParameterFile('output_folder', tr('Output folder:'), 
+#                    behavior=QgsProcessingParameterFile.Folder, defaultValue=default_output_folder))
+
+        # Option to compress the output
+        self.addParameter(QgsProcessingParameterBoolean('compress_output', tr('Compress output file copy (.zip)'), 
+                    defaultValue=False))
+
+        # Option to load the output into the project
+        self.addParameter(QgsProcessingParameterBoolean('load_output', tr('Open output files after executing the algorithm'), 
+                    defaultValue=True))
+
+    def processAlgorithm(self, parameters, context, feedback):
+        feedback = QgsProcessingMultiStepFeedback(1, feedback)
+
+        # Get the input layer as a feature source
+        layer = self.parameterAsSource(parameters, 'layer', context)
+        if layer is None:
+            raise QgsProcessingException(self.invalidSourceError(parameters, 'layer'))
+
+
+        # Get the field selected by the user
+        export_field = self.parameterAsString(parameters, 'export_field', context)
+        
+        # Get parameters        
+        output_folder = self.parameterAsFile(parameters, 'output_folder', context)
+        compress_output = self.parameterAsBoolean(parameters, 'compress_output', context)
+        load_output = self.parameterAsBoolean(parameters, 'load_output', context)
+
+        
+        # List to hold the output file names for loading later
+        output_files = []
+
+        # Processa as feições em lotes
+
+        features = list(layer.getFeatures())        
+        # Define o tamanho do lote
+        batch_size = 10 
+    
+        for batch_start in range(0, len(features), batch_size):
+         
+        # Iterates over the features and exports each one as an individual KML file.          
+            for i in range(batch_start, min(batch_start + batch_size, len(features))):
+                feature = features[i]
+                
+                # Uses the field value for naming or a default name
+                field_value = feature[export_field] if export_field else f"{layer.sourceName()}_{i+1}"
+
+                #Gets the geometry of the feature and ensures it is valid
+                geometry = feature.geometry()
+                if not geometry or geometry.isEmpty():
+                    feedback.pushInfo(f"Skipping feature {i+1}: No valid geometry found.")
+                    continue
+
+                geometries = [geometry] if not geometry.isMultipart() else geometry.asGeometryCollection()
+
+                # Processes each part of the geometry 
+                for j, singlepart_geometry in enumerate(geometries):            
+                    # Adjusts the file name for multipart
+                    part_suffix = f"_{j+1}" if geometry.isMultipart() else ""
+                    
+                    # Determines the geometry type of the feature
+                    geometry_type = singlepart_geometry.wkbType()
+                    
+                    # Creates a temporary layer depending on the geometry type
+                    if QgsWkbTypes.geometryType(geometry_type) == QgsWkbTypes.LineGeometry:
+                        temp_layer = QgsVectorLayer("LineString?crs=EPSG:4326", f"{field_value}{part_suffix}", "memory")
+                    elif QgsWkbTypes.geometryType(geometry_type) == QgsWkbTypes.PolygonGeometry:
+                        temp_layer = QgsVectorLayer("Polygon?crs=EPSG:4326", f"{field_value}{part_suffix}", "memory")
+                    else:
+                        raise QgsProcessingException(f"Unsupported geometry type: {QgsWkbTypes.displayString(geometry_type)}")
+
+                    #Adds the feature to the temporary layer
+                    temp_layer_data_provider = temp_layer.dataProvider()
+                    temp_layer_data_provider.addAttributes(layer.fields())
+                    
+                    # Updates the layer fields after adding the attributes
+                    temp_layer.updateFields()
+
+                    
+                    #Creates a new feature with the singlepart geometry
+                    new_feature = QgsFeature()
+                    new_feature.setGeometry(singlepart_geometry)
+                    new_feature.setAttributes(feature.attributes())
+                    temp_layer_data_provider.addFeature(new_feature)
+
+                    #Defines options to save the layer (writeAsVectorFormatV3)
+                    options = QgsVectorFileWriter.SaveVectorOptions()
+                    options.driverName = 'KML'
+                    options.fileEncoding = 'UTF-8'
+                    options.fieldNameSource = QgsVectorFileWriter.Original
+
+                    #Ensures that the correct coordinate transformation is used.
+                    transform_context = QgsProject.instance().transformContext()
+                    
+                    # Writes the feature to a KML file
+                    output_file = os.path.join(output_folder, f"{field_value}{part_suffix}.kml")
+                    error = QgsVectorFileWriter.writeAsVectorFormatV3(temp_layer, output_file, transform_context, options)
+
+                    if error[0] != QgsVectorFileWriter.NoError:
+                        raise QgsProcessingException(f"Error saving KML file: {error[0]}")
+
+                    feedback.pushInfo(f"File saved at {output_file}")
+                    
+                    #Removes the <Folder> tags and adds the <name> tag
+                    self.edit_kml_tags(output_file, f"{field_value}{part_suffix}")
+                    
+                    #Add file name for loading later
+                    output_files.append(output_file)
+
+                    #Del the temporary layer after use
+                    del temp_layer
+
+        #Compresses the files
+        if compress_output:
+            for output_file in output_files:
+                self.compress_files(output_file)
+
+        #Loads all KML files into the project at once
+        if load_output:
+            self.load_output_files(output_files)
+            
+        return {self.output_folder: output_folder}
+        
+        
+    def load_output_files (self, output_files):  
+       #Using the addMapLayers method to reduce the number of calls to QGIS when many layers need to be loaded
+       layers_to_add = []
+       for output_file in output_files:
+           layers_to_add.append(QgsVectorLayer(output_file, os.path.basename(output_file), "ogr"))
+       QgsProject.instance().addMapLayers(layers_to_add)
+
+
+    def compress_files(self, output_file):
+        zip_output_file = output_file + ".zip"
+        with zipfile.ZipFile(zip_output_file, 'w') as zipf:
+            zipf.write(output_file, os.path.basename(output_file))
+
+    def edit_kml_tags(self, kml_file, field_value):
+        with open(kml_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        with open(kml_file, 'w', encoding='utf-8') as file:
+            for line in lines:
+                if '<Folder>' in line or '</Folder>' in line:
+                    continue
+                if '<Placemark>' in line:
+                    file.write(line)
+                    file.write(f"<name>{field_value}</name>\n")
+                else:
+                    file.write(line)
+
+    def name(self):
+        return tr("Export KML to RPA")
+
+    def displayName(self):
+        return tr("Export KML to RPA")
+
+    def group(self):
+        return tr("Emi Functions")
+
+    def groupId(self):
+        return ""
+
+    def createInstance(self):
+        return emiToolsExportKmlRpa()
+
---
+++ zip_build/emi_tools/i18n/emiTools_pt_BR.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="58"/>
+        <source>Layer name:</source>
+        <translation>Nome da camada:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_kml_rpa.py" line="64"/>
+        <source>Field to use for export file names:</source>
+        <translation>Campo a ser utilizado para os nomes dos arquivos de exportação:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="68"/>
+        <source>Output folder:</source>
+        <translation>Pasta de saída:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="77"/>
+        <source>Compress output file copy (.zip)</source>
+        <translation>Compactar cópia do arquivo de saída (.zip)</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="80"/>
+        <source>Open output files after executing the algorithm</source>
+        <translation>Abrir arquivos de saída após a execução do algoritmo</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_kml_rpa.py" line="196"/>
+        <source>Export KML to RPA</source>
+        <translation>Exportar KML para RPA</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="413"/>
+        <source>Emi Functions</source>
+        <translation>Funções Emi</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="61"/>
+        <source>Embargo term field:</source>
+        <translation>Campo do termo de embargo:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="64"/>
+        <source>Embargo term series field:</source>
+        <translation>Campo da série do termo de embargo:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="71"/>
+        <source>Output file format:</source>
+        <translation>Formato do arquivo de saída:</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="74"/>
+        <source>Export all features to a single file</source>
+        <translation>Exportar todas as feições para um único arquivo</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="147"/>
+        <source>More than one feature associated with the embargo term number was found: {list(duplicated_tads.keys())}. Feature count: {len(extracted_features)}.</source>
+        <translation>Mais de uma feição associada ao número do termo de embargo foi encontrada: {list(duplicated_tads.keys())}. Contagem de feições: {len(extracted_features)}.</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="236"/>
+        <source>Compressed files: {zip_output_file}</source>
+        <translation>Arquivos compactados: {zip_output_file}</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="258"/>
+        <source>Export Terms to Sicafi</source>
+        <translation>Exportar Termos para o Sicafi</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_expressions.py" line="153"/>
+        <source>Invalid number. Please provide an 11-digit numeric string.</source>
+        <translation>Número inválido. Por favor, forneça uma string numérica de 11 dígitos.</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="66"/>
+        <source>Stamp SVG Image</source>
+        <translation>Imagem SVG a ser inserido na imagem</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="67"/>
+        <source>Main text to be inserted into the image</source>
+        <translation>Texto principal a ser inserido na imagem</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="68"/>
+        <source>Secondary text to be inserted into the image</source>
+        <translation>Texto secundário a ser inserido na imagem</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="84"/>
+        <source>Font color</source>
+        <translation>Cor da fonte</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="85"/>
+        <source>Font size</source>
+        <translation>Tamanho da fonte</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="97"/>
+        <source>Position of text and image</source>
+        <translation>Posição do texto e da imagem</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="104"/>
+        <source>Output folder</source>
+        <translation>Pasta de saída</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="150"/>
+        <source>Image saved at {output_image_path}</source>
+        <translation>Imagem salva em {output_image_path}</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="172"/>
+        <source>No valid raster layer provided.</source>
+        <translation>Nenhuma camada raster válida fornecida.</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="138"/>
+        <source>Failed to load input image.</source>
+        <translation>Falha ao carregar a imagem de entrada.</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="263"/>
+        <source>Failed to load SVG file: {svg_file_path}</source>
+        <translation>Falha ao carregar o arquivo SVG: {svg_file_path}</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="410"/>
+        <source>Insert stamp on image</source>
+        <translation>Inserir carimbo na imagem</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="82"/>
+        <source>Select Font</source>
+        <translation>Selecione a fonte</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="90"/>
+        <source>Bottom Left</source>
+        <translation>Inferior Esquerda</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="91"/>
+        <source>Bottom Right</source>
+        <translation>Inferior Direita</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="92"/>
+        <source>Top Left</source>
+        <translation>Superior Esquerda</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="93"/>
+        <source>Top Right</source>
+        <translation>Superior Direita</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="334"/>
+        <source>Failed to tag {tag}: {str(e)}</source>
+        <translation>Falha ao inserir {tag}: {str(e)}</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_imagem_stamp_rpa.py" line="65"/>
+        <source>Input Images</source>
+        <translation>Imagens de Entrada</translation>
+    </message>
+    <message>
+        <location filename="../emi_tools_export_terms.py" line="104"/>
+        <source>The embargo term field and the embargo term series field must not be the same. Please select different fields.</source>
+        <translation>O campo do termo de embargo e o campo da série do termo de embargo não podem ser iguais. Por favor, selecione campos diferentes.</translation>
+    </message>
+</context>
+<context>
+    <name>emiToolsProvider</name>
+    <message>
+        <location filename="../emi_tools_provider.py" line="77"/>
+        <source>Emi Tools</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
