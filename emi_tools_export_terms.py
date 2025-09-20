7f740dfccbdd6ca91e768a910ae329039d133f7a
alexandreparente <alexandre.parente@gmail.com>
2025-09-20 10:55:31 -0300

    - Fixes memory error in the export_individual_files function. - Centralizes file format logic to remove code duplication. - Standardizes the use of the writeAsVectorFormatV3 function for all file writing.

--- emi_tools_export_terms.py
+++ emi_tools_export_terms.py
@@ -26,24 +26,22 @@ __author__ = 'Alexandre Parente Lima'
 __date__ = '2024-10-10'
 __copyright__ = '(C) 2024 by Alexandre Parente Lima'
 
-# This will get replaced with a git SHA1 when you do a git archive
-
 __revision__ = '$Format:%H$'
 
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing, QgsVectorFileWriter,
-                       QgsProcessingAlgorithm, 
+                       QgsProcessingAlgorithm,
                        QgsProcessingParameterFeatureSource,
-                       QgsProcessingParameterField, 
+                       QgsProcessingParameterField,
                        QgsProcessingParameterFolderDestination,
-                       QgsProcessingParameterEnum, 
+                       QgsProcessingParameterEnum,
                        QgsProcessingParameterBoolean,
-                       QgsProcessingException, 
+                       QgsProcessingException,
                        QgsProject, QgsVectorLayer,
                        QgsProcessingMultiStepFeedback,
                        QgsProcessingFeatureSource,
                        QgsWkbTypes)
-                       
+
 import os
 import zipfile
 import tempfile
@@ -51,107 +49,75 @@ from datetime import datetime
 
 from .emi_tools_util import tr
 
-class emiToolsExportTerms(QgsProcessingAlgorithm):
 
+class emiToolsExportTerms(QgsProcessingAlgorithm):
     OUTPUT_FOLDER = 'OUTPUT_FOLDER'
+    FORMAT_INFO = {
+        0: {'driver': 'ESRI Shapefile', 'ext': 'shp', 'zip_exts': ['shp', 'shx', 'dbf', 'prj', 'cpg']},
+        1: {'driver': 'GPKG', 'ext': 'gpkg', 'zip_exts': ['gpkg']},
+        2: {'driver': 'KML', 'ext': 'kml', 'zip_exts': ['kml']}
+    }
 
     def initAlgorithm(self, config=None):
-        # Input layer parameter   
         self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                'layer',
-                tr('Input layer'),
-                [QgsProcessing.TypeVectorPolygon]
-            )
+            QgsProcessingParameterFeatureSource('layer', tr('Input layer'), [QgsProcessing.TypeVectorPolygon])
         )
         
         # Parameter to select the field for the embargo term number
         self.addParameter(
-            QgsProcessingParameterField(
-                'num_tei_field',
-                tr('Embargo term field'),
-                parentLayerParameterName='layer',
-                type=QgsProcessingParameterField.String,
-                defaultValue='numero_tad'
-            )
+            QgsProcessingParameterField('num_tei_field', tr('Embargo term field'), parentLayerParameterName='layer',
+                                        type=QgsProcessingParameterField.String, defaultValue='numero_tad')
         )
 
         # Parameter to select the field for the embargo term series        
         self.addParameter(
-            QgsProcessingParameterField(
-                'serie_tei_field',
-                tr('Embargo term series field'),
-                parentLayerParameterName='layer',
-                type=QgsProcessingParameterField.String,
-                defaultValue='serie_tad'
-            )
+            QgsProcessingParameterField('serie_tei_field', tr('Embargo term series field'),
+                                        parentLayerParameterName='layer', type=QgsProcessingParameterField.String,
+                                        defaultValue='serie_tad')
         )
         
         # Setting the default output folder to the user's home directory
         self.addParameter(
-            QgsProcessingParameterFolderDestination(
-                self.OUTPUT_FOLDER,
-                tr('Output folder')
-            )
+            QgsProcessingParameterFolderDestination(self.OUTPUT_FOLDER, tr('Output folder'))
         )
 
         # Parameter to choose the output format        
         self.addParameter(
-            QgsProcessingParameterEnum(
-                'output_format',
-                tr('Output file format:'),
-                options=['ESRI Shapefile', 'GeoPackage', 'Keyhole Markup Language'],
-                defaultValue=0
-            )
+            QgsProcessingParameterEnum('output_format', tr('Output file format:'),
+                                       options=[info['driver'] for info in self.FORMAT_INFO.values()], defaultValue=0)
         )
         
         # Add parameter to export all features to a single file        
         self.addParameter(
-            QgsProcessingParameterBoolean(
-                'export_all_to_single',
-                tr('Export all features to a single file'),
-                defaultValue=False
-            )
+            QgsProcessingParameterBoolean('export_all_to_single', tr('Export all features to a single file'),
+                                          defaultValue=False)
         )
         
         # Parameter to compress the output file
         self.addParameter(
-            QgsProcessingParameterBoolean(
-                'compress_output',
-                tr('Compress output file copy (.zip)'),
-                defaultValue=False
-            )
+            QgsProcessingParameterBoolean('compress_output', tr('Compress output file copy (.zip)'), defaultValue=False)
         )
         
         # Parameter to load the output file        
         self.addParameter(
-            QgsProcessingParameterBoolean(
-                'load_output',
-                tr('Open output files after executing the algorithm'),
-                defaultValue=True
-            )
+            QgsProcessingParameterBoolean('load_output', tr('Open output files after executing the algorithm'),
+                                          defaultValue=True)
         )
 
     def processAlgorithm(self, parameters, context, feedback):
         feedback = QgsProcessingMultiStepFeedback(1, feedback)
-
-        # Get the input layer as a feature source
         layer = self.parameterAsSource(parameters, 'layer', context)
         if layer is None:
             raise QgsProcessingException(self.invalidSourceError(parameters, 'layer'))
 
-        # Get parameters
         extracted_features = list(layer.getFeatures())
-
         num_tei_field = parameters['num_tei_field']
         serie_tei_field = parameters['serie_tei_field']
         export_all_to_single = self.parameterAsBoolean(parameters, 'export_all_to_single', context)
         output_format = self.parameterAsEnum(parameters, 'output_format', context)
         compress_output = self.parameterAsBoolean(parameters, 'compress_output', context)
-        
-	# Get output folder
+
         output_folder = self.parameterAsString(parameters, self.OUTPUT_FOLDER, context)
-        
         if not output_folder:
             output_folder = tempfile.mkdtemp()  # Create a secure temporary folder
             
@@ -170,15 +136,15 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
 
         # Rename the fields
         self.rename_fields(temp_layer, num_tei_field, serie_tei_field)
-      
+
         output_files = []
         today_str = "lote" + datetime.today().strftime('%Y%m%d')
 
         # Export all to a single file or individual files
         if export_all_to_single:
-            output_files.extend(self.export_single_file(temp_layer, output_folder, today_str, output_format,feedback))
+            output_files.extend(self.export_single_file(temp_layer, output_folder, today_str, output_format, feedback))
         else:
-            output_files.extend(self.export_individual_files(temp_layer, output_folder, output_format,feedback))
+            output_files.extend(self.export_individual_files(temp_layer, output_folder, output_format, feedback))
 
         # Compress files if option is checked
         if compress_output:
@@ -189,6 +155,7 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
             self.load_output_files(output_files)
 
         return {self.OUTPUT_FOLDER: output_folder}
+
 
     def temp_layer(self, layer, extracted_features, context):
         # Checks if the layer is a QgsProcessingFeatureSource
@@ -222,7 +189,8 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
 
     def check_duplicates(self, extracted_features, num_tei_field):
         tad_numbers = [f[num_tei_field] for f in extracted_features]
-        duplicated_tads = {tad_number: tad_numbers.count(tad_number) for tad_number in set(tad_numbers) if tad_numbers.count(tad_number) > 1}
+        duplicated_tads = {tad_number: tad_numbers.count(tad_number) for tad_number in set(tad_numbers) if
+                           tad_numbers.count(tad_number) > 1}
         if duplicated_tads:
             raise QgsProcessingException(tr(f"Duplicate embargo term numbers found: {list(duplicated_tads.keys())}"))
 
@@ -230,11 +198,8 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
         # Fields that should be kept
         fields_to_keep = [num_tei_field, serie_tei_field]
         provider = temp_layer.dataProvider()
-
-        # Collect indexes of fields to be removed
-        fields_to_remove = [temp_layer.fields().indexOf(field.name()) for field in temp_layer.fields() if field.name() not in fields_to_keep]
-        
-        # Remove the fields
+        fields_to_remove = [temp_layer.fields().indexOf(field.name()) for field in temp_layer.fields() if
+                            field.name() not in fields_to_keep]
         if fields_to_remove:
             provider.deleteAttributes(fields_to_remove)
             
@@ -258,63 +223,61 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
         # Update the layer's fields
         temp_layer.updateFields()
 
-    def export_single_file(self, layer, output_folder, today_str, output_format,feedback):
-        output_files = []
-        output_file = os.path.join(output_folder, f"TEI_{today_str}_sicafi.{self.get_extension(output_format)}")
-        QgsVectorFileWriter.writeAsVectorFormat(layer, output_file, "UTF-8", layer.crs(), self.get_output_format_string(output_format))
-        output_files.append(output_file)
-        feedback.pushInfo(tr(f"Save file: {output_file}"))  
-          
-        return output_files
-        
+
+    def export_single_file(self, layer, output_folder, today_str, output_format, feedback):
+        extension = self.get_extension(output_format)
+        output_file = os.path.join(output_folder, f"TEI_{today_str}_sicafi.{extension}")
+
+        options = QgsVectorFileWriter.SaveVectorOptions()
+        options.driverName = self.get_output_format_string(output_format)
+        options.fileEncoding = "UTF-8"
+
+        error = QgsVectorFileWriter.writeAsVectorFormatV3(layer,
+                                                          output_file,
+                                                          QgsProject.instance().transformContext(),
+                                                          options)
+
+        if error[0] == QgsVectorFileWriter.NoError:
+            feedback.pushInfo(tr(f"Save file: {output_file}"))
+            return [output_file]
+        else:
+            feedback.reportError(tr(f"Error saving file {output_file}: {error[1]}"))
+            return []
+
     def export_individual_files(self, layer, output_folder, output_format, feedback):
         output_files = []
+        driver_name = self.get_output_format_string(output_format)
+        extension = self.get_extension(output_format)
+        num_tei_index = layer.fields().indexOf('NUM_TEI')
+        if num_tei_index == -1:
+            raise QgsProcessingException(tr("Field 'NUM_TEI' not found in the layer after renaming."))
 
-        # Load all features at once
-        features = list(layer.getFeatures())  
-        total_features = len(features)
-        
-        # Use a set to track processed TAD numbers
-        processed_tad_numbers = set()
-    
-        for i, feature in enumerate(features):
-        
-            output_layer = QgsVectorLayer("Polygon?crs=EPSG:4326", f"Extracted_{i + 1}", "memory")
-            provider = output_layer.dataProvider()
-            provider.addAttributes(layer.fields())    
-            output_layer.updateFields()
-            
-            # Retrieve the value of 'NUM_TEI' to generate the file name
-            tad_number = feature[layer.fields().indexOf('NUM_TEI')]    
-            
-            # Add the feature to the temporary layer
-            provider.addFeature(feature)
-    
-            # Generate the output file path
-            output_file = os.path.join(output_folder, f"TEI_{tad_number}_sicafi.{self.get_extension(output_format)}")
-            
-            # Export the temporary layer as a file
-            QgsVectorFileWriter.writeAsVectorFormat(output_layer, output_file, "UTF-8", output_layer.crs(), self.get_output_format_string(output_format))
-            output_files.append(output_file)
-            
-            # Print the saved file info
-            feedback.pushInfo(f"Saved file: {output_file}")
+        for feature in layer.getFeatures():
+            if feedback.isCanceled(): break
+            tad_number = feature.attribute(num_tei_index)
+            if not tad_number:
+                feedback.pushWarning(tr(f"Skipping feature {feature.id()} due to empty 'NUM_TEI' value."))
+                continue
 
-            # Clear/Create the temporary layer for each feature in the batch
-            output_layer = None
+            output_file = os.path.join(output_folder, f"TEI_{tad_number}_sicafi.{extension}")
+            options = QgsVectorFileWriter.SaveVectorOptions()
+            options.driverName = driver_name
+            options.fileEncoding = "UTF-8"
+            options.filterFids = [feature.id()]
+            error = QgsVectorFileWriter.writeAsVectorFormatV3(layer, output_file,
+                                                              QgsProject.instance().transformContext(), options)
 
-        #Apparently, this is causing a memory error
-        # #Print the strings (files) generated
-        #for file in output_files:
-        #    feedback.pushInfo(f"Saved file: {file}")
-       
-        # Print the total number of saved files
-        feedback.pushInfo(f"Total number of saved files: {len(output_files)}")
-               
+            if error[0] == QgsVectorFileWriter.NoError:
+                output_files.append(output_file)
+                feedback.pushInfo(tr(f"Saved file: {output_file}"))
+            else:
+                feedback.reportError(tr(f"Error saving file for TEI {tad_number}: {error[1]}"))
+
+        feedback.pushInfo(tr(f"Total number of saved files: {len(output_files)}"))
         return output_files
 
-
     def load_output_files(self, list_output_files):
+
         layers_to_add = []
         for output_file in list_output_files:
             layer_name = os.path.basename(output_file)
@@ -324,52 +287,31 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
             layers_to_add.append(layer)
         QgsProject.instance().addMapLayers(layers_to_add)
 
-
     def compress_files(self, output_files, output_format, output_folder, feedback):
-        #Define the associated extensions based on the output format
-        if output_format == 0:  # ESRI Shapefile
-            associated_extensions = ['shp', 'shx', 'dbf', 'prj', 'cpg']
-            file_extension = 'shp'
-        elif output_format == 1:  # GeoPackage
-            associated_extensions = ['gpkg']
-            file_extension = 'gpkg'
-        elif output_format == 2:  # KML
-            associated_extensions = ['kml']
-            file_extension = 'kml'
-        
+        format_info = self.FORMAT_INFO.get(output_format)
+        if not format_info: return
+
+        associated_extensions = format_info['zip_exts']
+        file_extension = format_info['ext']
+
         for output_file in output_files:
             base_name = os.path.splitext(os.path.basename(output_file))[0]
-            
-            #Add the file extension before the '.zip' 
             zip_output_file = os.path.join(output_folder, f"{base_name}_{file_extension}.zip")
-        
-            #Identify the associated files that need to be compressed 
-            associated_files = [os.path.join(output_folder, f"{base_name}.{ext}") for ext in associated_extensions if os.path.exists(os.path.join(output_folder, f"{base_name}.{ext}"))]
-            
-            #If there are associated files, create the .zip file
+            associated_files = [os.path.join(output_folder, f"{base_name}.{ext}") for ext in associated_extensions if
+                                os.path.exists(os.path.join(output_folder, f"{base_name}.{ext}"))]
+
             if associated_files:
                 with zipfile.ZipFile(zip_output_file, 'w') as zipf:
                     for file in associated_files:
                         zipf.write(file, os.path.basename(file))
-            
-                # Notify that the file has been compressed
                 feedback.pushInfo(tr(f"Compressed files: {zip_output_file}"))
-    
+
     def get_extension(self, output_format):
-        if output_format == 0:
-            return "shp"
-        elif output_format == 1:
-            return "gpkg"
-        elif output_format == 2:
-            return "kml"
+        return self.FORMAT_INFO.get(output_format, {}).get('ext', 'tmp')
 
     def get_output_format_string(self, output_format):
-        if output_format == 0:
-            return "ESRI Shapefile"
-        elif output_format == 1:
-            return "GPKG"
-        elif output_format == 2:
-            return "KML"
+        return self.FORMAT_INFO.get(output_format, {}).get('driver', 'unknown')
+
 
     def name(self):
         return "emiToolsExportTerms"
@@ -391,4 +333,3 @@ class emiToolsExportTerms(QgsProcessingAlgorithm):
 
     def createInstance(self):
         return emiToolsExportTerms()
-        
